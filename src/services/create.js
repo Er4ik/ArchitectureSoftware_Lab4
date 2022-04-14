083d6ec907f0e868a7556760b8f80acf58347aaf
hack_er <70893070+Er4ik@users.noreply.github.com>
2022-04-14 17:16:42 +0300

    feature/made basic logic creation task and write to db file

--- src/services/create.js
+++ src/services/create.js
@@ -1,7 +1,27 @@
 "use strict"
 
+const fs = require("fs");
+const readlineSync = require("readline-sync");
+const { pathToDB } = require("../common/pathToDB");
+const { questionToFillTask } = require("../common/question.common");
+
 const createTask = async () => {
-    return;
+    try {
+        const taskToDB = {};
+
+        Object.keys(questionToFillTask).forEach((key) => {
+            taskToDB[key] = readlineSync.question(questionToFillTask[key]);
+        });
+
+        fs.writeFile(pathToDB.path, JSON.stringify(taskToDB), (err) => {
+            if (err) throw err;
+            console.log("---> Task was successfully created <---");
+        });
+
+        return;
+    } catch (err) {
+        throw new Error(`Error create task --> ${err}`);
+    }
 }
 
 if (require.main === module) {
